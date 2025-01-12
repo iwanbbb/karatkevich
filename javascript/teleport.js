4f3fb069163b06521299ce11db57a5e8396699f5
Iwan Bbb <iwanbaburevich@gmail.com>
2025-01-12 15:25:41 +0300

    final 12/01/2025 15:23

--- javascript/teleport.js
+++ javascript/teleport.js
@@ -1,7 +1,17 @@
 const btns = document.querySelector('.nav__list')
-let c = 0
+const btns2 = document.querySelector('#nav')
+
 
 btns.addEventListener('click', (e) => {
+    let currId = e.target.getAttribute('data-tp');
+    let currBlock = document.querySelector(`#${currId}`);
+    console.log(currBlock)
+    currBlock.scrollIntoView ({
+        behavior: 'smooth'
+    })
+})
+
+btns2.addEventListener('click', (e) => {
     let currId = e.target.getAttribute('data-tp');
     let currBlock = document.querySelector(`#${currId}`);
     console.log(currBlock)
